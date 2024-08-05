0b7dd37908f2ccc7501bb30edeb440f1bd0cae4d
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 04:00:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// part 36 11 question 41
+// magician make array
+var magician_list = ["David copperfield", "Cris Angel", "David Blaine", "Penn & Teller "];
+for (var i = 0; i < magician_list.length; i++) {
+    function call_magician(list) {
+        console.log(list);
+    }
+    console.log(magician_list);
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+// part 36 11 question 41
+// magician make array
+
+let magician_list: string[] = ["David copperfield","Cris Angel","David Blaine","Penn & Teller "]
+
+for(let i=0; i<magician_list.length; i++){
+    function call_magician(list:string,){
+        console.log(list)
+    }
+    console.log(magician_list)
+}
